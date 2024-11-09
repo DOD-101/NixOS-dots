68b7fbdb719894db20000547f65926b5cbb2c98a
David Thievon <pkfan@gmail.com>
2024-11-09 16:20:52 +0100

    feat(nix101-2): add basic config for nix101-2

---
+++ hosts/nix101-2/configuration.nix
@@ -0,0 +1,126 @@
+{
+  pkgs,
+  inputs,
+  lib,
+  config,
+  ...
+}:
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernelParams = [
+    "quiet"
+    "udev.log_priority=3"
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+  programs.nix-ld.enable = true;
+
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh.enable = true;
+
+  networking.hostName = "nix101-2"; # Define your hostname.
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "America/Halifax";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "de-latin1";
+  };
+
+  programs.uwsm.enable = true;
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  services.udisks2.enable = true;
+
+  # Enable config modules
+  sound-config.enable = true;
+
+  users.users.david = {
+    isNormalUser = true;
+    home = "/home/david";
+    # TODO: this needs to be changed
+    hashedPassword = "$y$j9T$0QvrKa4enFuufCu14r0NC/$Xvij.zytnbXdHt64yoJZxA4JF99LtFuhNsJMzxM1md1";
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "plugdev"
+      "input"
+    ];
+  };
+
+  home-manager = {
+    extraSpecialArgs = {
+      inherit inputs;
+    };
+    users = {
+      "david" = import ./home.nix;
+    };
+    backupFileExtension = "bck";
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    stow
+    gccgo14
+    gnumake42
+    wirelesstools
+  ];
+
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = true;
+      AllowUsers = null;
+      UseDns = true;
+    };
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
---
+++ hosts/nix101-2/home.nix
@@ -0,0 +1,42 @@
+{
+  pkgs,
+  inputs,
+  config,
+  ...
+}:
+{
+  imports = [
+    ../../modules/home-manager
+    ../../themes
+  ];
+
+  home.username = "server";
+  home.homeDirectory = "/home/server";
+
+  theme.ocean-breeze.enable = true;
+
+  zsh-config.enable = true;
+  fastfetch-config.enable = true;
+  btop-config.enable = true;
+
+  dconf = {
+    enable = true;
+    settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+  };
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  # home.packages = with pkgs; [ ];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+}
