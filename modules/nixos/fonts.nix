c38bc3f259cfd6ecd61befca9e423f500b732af5
David Thievon <pdkfan@gmail.com>
2024-12-07 14:07:29 +0100

    feat(nixpkgs): update nixpkgs and adjust acordingly
    Adjustments:
    
    - nerd-font names changed

--- modules/nixos/fonts.nix
+++ modules/nixos/fonts.nix
@@ -15,12 +15,9 @@
     ];
 
     fonts.packages = with pkgs; [
-      (nerdfonts.override {
-        fonts = [
-          "FiraCode"
-          "JetBrainsMono"
-        ];
-      })
+      nerd-fonts.fira-code
+      nerd-fonts.jetbrains-mono
+      noto-fonts-color-emoji
       corefonts
     ];
   };
