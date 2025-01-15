272c19d53a9def3a5caf0bd0fb2faca6330ef2e9
David Thievon <pdkfan@gmail.com>
2025-01-15 09:44:25 +0100

    fix(zen): change zen-browser input
    The last one was abandoned. (again)

--- flake.nix
+++ flake.nix
@@ -14,7 +14,10 @@
     # since the cursor are broken on the newest nixpkgs
     cursor_nixpkgs.url = "github:nixos/nixpkgs/574d1eac1c200690e27b8eb4e24887f8df7ac27c";
 
-    zen-browser.url = "github:Crazyroostereye1/zen-browser-flake";
+    zen-browser = {
+      url = "github:youwen5/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     home-manager = {
       url = "github:nix-community/home-manager";
