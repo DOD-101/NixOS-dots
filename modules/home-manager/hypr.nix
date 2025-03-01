98104d56ed638f76d1644a010140bf0bcdfb3406
David Thievon <pdkfan@gmail.com>
2025-03-01 13:25:14 +0100

    refactor(hypr): use nixpkgs version of config-store

--- modules/home-manager/hypr.nix
+++ modules/home-manager/hypr.nix
@@ -30,33 +30,7 @@
       grim
       cliphist
       brightnessctl
-
-      (rustPlatform.buildRustPackage rec {
-        pname = "config-store";
-        version = "1.0.0";
-
-        src = pkgs.fetchFromGitHub {
-          owner = "DOD-101";
-          repo = pname;
-          rev = "refs/tags/v${version}";
-          sha256 = "sha256-dmFIB9tVI5/hnI+VKeawFzKi6UJrRis0tpeQE5a5dGU=";
-        };
-
-        cargoHash = "sha256-tEhk6vDan5uaP1vvKanB+juKsGJndrJPP67kudds24s=";
-
-        meta = {
-          description = "A simple key-value store designed to be used from shell scripts written in rust";
-          homepage = "https://github.com/DOD-101/config-store";
-          mainProgram = "config-store";
-          license = with lib.licenses; [
-            mit
-            asl20
-          ];
-          maintainers = with lib.maintainers; [ dod-101 ];
-
-          platforms = lib.platforms.linux;
-        };
-      })
+      config-store
     ];
 
     # hyprland
