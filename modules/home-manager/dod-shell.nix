6897db1ff8cf5d3ffc7029e7db0ac8079ed4f635
David Thievon <david.thievon@proton.me>
2026-01-29 23:19:50 +0100

    feat(dod-shell): add missing apps to launcher

--- modules/home-manager/dod-shell.nix
+++ modules/home-manager/dod-shell.nix
@@ -43,77 +43,79 @@ in
       enable = true;
       removed-components = cfg.removed-components;
       scss.text = colors + "\n" + builtins.readFile config.theme.dod-shell;
-      # TODO: Rework this
-      config.config =
-        if
-          lib.attrsets.hasAttrByPath [
-            "launcher"
-            "launch_mode"
-            "apps"
-          ] cfg.settings
-        then
-          cfg.settings
-        else
-          lib.attrsets.recursiveUpdate cfg.settings {
-            launcher = {
-              max_results = 30;
+      config.config = lib.attrsets.recursiveUpdate cfg.settings {
 
-              launch_mode.apps = [
-                {
-                  cmd = "foot";
-                  name = "Foot";
-                }
-                {
-                  cmd = "kitty -e spotify_player";
-                  name = "Spotify Player";
-                }
-                {
-                  cmd = "kitty";
-                  name = "Kitty";
-                }
-                {
-                  cmd = "steam";
-                  name = "Steam";
-                }
-                {
-                  cmd = "vesktop";
-                  name = "Vesktop";
-                }
-                {
-                  cmd = "heroic";
-                  name = "Heroic Launcher";
-                }
-                {
-                  cmd = "signal-desktop";
-                  name = "Signal";
-                }
-                {
-                  cmd = "prismlauncher";
-                  name = "Prism";
-                }
-                {
-                  cmd = "drawio";
-                  name = "Draw.io";
-                }
-                {
-                  cmd = "zen";
-                  name = "Zen Browser";
-                }
-                {
-                  cmd = "thunderbird";
-                  name = "Thunderbird";
-                }
-                {
-                  cmd = "xournalpp";
-                  name = "Xournal++";
-                }
-                {
-                  cmd = "teams-for-linux";
-                  name = "Teams for Linux";
-                }
-              ];
-            };
-          };
+        launcher = {
+          max_results = 30;
+          # NOTE: Upstream changes needed to get rid of this unnecessarily long list
+          launch_mode.apps = [
+            {
+              cmd = "foot";
+              name = "Foot";
+            }
+            {
+              cmd = "kitty -e spotify_player";
+              name = "Spotify Player";
+            }
+            {
+              cmd = "kitty";
+              name = "Kitty";
+            }
+            {
+              cmd = "steam";
+              name = "Steam";
+            }
+            {
+              cmd = "vesktop";
+              name = "Vesktop";
+            }
+            {
+              cmd = "heroic";
+              name = "Heroic Launcher";
+            }
+            {
+              cmd = "signal-desktop";
+              name = "Signal";
+            }
+            {
+              cmd = "prismlauncher";
+              name = "Prism";
+            }
+            {
+              cmd = "drawio";
+              name = "Draw.io";
+            }
+            {
+              cmd = "zen";
+              name = "Zen Browser";
+            }
+            {
+              cmd = "thunderbird";
+              name = "Thunderbird";
+            }
+            {
+              cmd = "xournalpp";
+              name = "Xournal++";
+            }
+            {
+              cmd = "teams-for-linux";
+              name = "Teams for Linux";
+            }
+            {
+              cmd = "keepassxc";
+              name = "Keepass XC";
+            }
+            {
+              cmd = "libreoffice";
+              name = "Libre Office";
+            }
+            {
+              cmd = "inkscape";
+              name = "Inkscape";
+            }
+          ];
+        };
+      };
     };
   };
 }
