3d363ba51a11893c3ec929a152fa2fc4a15d58cf
David Thievon <david.thievon@proton.me>
2026-05-22 23:52:33 +0200

    feat(fonts): add fontpreview
    - also wrap it for config / theming

--- modules/home-manager/fonts.nix
+++ modules/home-manager/fonts.nix
@@ -5,27 +5,47 @@
   ...
 }:
 {
-  options = {
-    font-config.enable = lib.mkEnableOption "enable font config";
+
+  options.font-config = {
+    enable = lib.mkEnableOption "enable font config";
+    fontpreview = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "if fontpreview should also be installed";
+    };
   };
 
   config = lib.mkIf config.font-config.enable {
     fonts = {
-      enableDefaultPackages = true;
-      antialiasing = true;
-      subpixelRendering = "rgb";
-      defaultFonts = {
-        inherit (config.theme.fonts) monospace sansSerif serif;
-        emoji = [ "NotoColorEmoji" ];
+      fontconfig = {
+        antialiasing = true;
+        subpixelRendering = "rgb";
+        defaultFonts = {
+          inherit (config.theme.fonts) monospace sansSerif serif;
+          emoji = [ "NotoColorEmoji" ];
+        };
       };
+    };
 
-      packages = with pkgs; [
+    home.packages =
+      with pkgs;
+      [
         nerd-fonts.fira-code
         nerd-fonts.jetbrains-mono
         nerd-fonts.recursive-mono
         noto-fonts-color-emoji
         corefonts
-      ];
-    };
+      ]
+      ++ lib.optional config.font-config.fontpreview (
+        writeShellScriptBin "fontpreview" ''
+          export FONTPREVIEW_PREVIEW_TEXT="ABCDEFGHIJKLM\nNOPQRSTUVWXYZ\nabcdefghijklm\nnopqrstuvwxyz\n1234567890\n!@$\%(){}[]\n>=|=<!="
+          export FONTPREVIEW_BG_COLOR=${config.theme.color.background}
+          export FONTPREVIEW_FG_COLOR=${config.theme.color.foreground}
+          export FONTPREVIEW_FONT_SIZE=57
+          export FONTPREVIEW_SIZE=798x548
+
+          ${fontpreview}/bin/fontpreview $@
+        ''
+      );
   };
 }
